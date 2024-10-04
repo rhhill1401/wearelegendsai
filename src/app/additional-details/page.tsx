c8f9e75f7f608347f745ad261407993137c5c616
Terry <rthill49@gmail.com>
2024-10-04 16:12:39 -0500

    removed additional details

--- src/app/additional-details/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdditionalDetailsForm from '@/components/AdditionalDetailsForm';
-
-export default function AdditionalDetailsPage() {
-	return (
-		<div className='container mx-auto py-8'>
-			<AdditionalDetailsForm />
-		</div>
-	);
-}
